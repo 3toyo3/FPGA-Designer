6a42782058d1f2b73a4391c5b702ebe3386aad76
yt <o@dm.com>
2023-11-10 13:53:52 -0500

    bitstream initialized in FPGAinitializer

--- FPGAinitializer.py
+++ FPGAinitializer.py
@@ -2,7 +2,7 @@ import numpy as np
 from os.path import exists
 # This program sets up the FPGA based on user input
 
-#TODO, pass all into seperate functions so that bitstreaming is easier as well as set up initial storage structure
+bits=['I'] #for bitstream
 
 def main():
 	bool newDesign = False
@@ -14,18 +14,21 @@ def main():
 		if LUTS_num.isnumeric(): #check that is a number
 			if int(LUTS_num)>0: #check that is greater than 0
 				LUTS_num = int(LUTS_num)
+				bits.append(LUTS_num) #for bitstream
 				break
 		print("Input must be a integer that is greater than 0.") #TODO check that this doesnt print if correct input
 	#LUTS type
 	while True:
 		print("What type of LUTs are you using?")
-		print("A: 4-input  B: 6-input")
+		print("4: 4-input  6: 6-input")
 		LUTS_type=input()
-		if LUTS_type == 'A': #TODO check that char comparison works for string input
+		if LUTS_type == '4': #TODO check that char comparison works for string input
 			LUTS_type=4
+			bits.append(LUTS_type)
 			break
-		elif LUTS_type == 'B': #TODO line 24 comment
+		elif LUTS_type == '6': #TODO line 24 comment
 			LUTS_type=6
+			bits.append(LUTS_type)
 			break
 		else:
 			print("Invalid input. Please pick option A or B.")
@@ -36,13 +39,17 @@ def main():
 		connect_type=input()
 		if connect_type=='A': #TODO line 24 comment
 			connect_type=1
+			bits.append(connect_type)
 			break
 		elif connect_type=='B': #TODO line 24 comment
 			connect_type=2
+			bits.append(connect_type)
 			#TODO File input here
 			print("This program currently doesn't support partial connection between LUTS.")
 		else:
 			print("Invalid input. Please pick option A or B")
+		#TODO specify connection
+		#bits.append(connection_detail)
 	# I/O
 	while True:
 		input_num=input("How many inputs?")
@@ -51,6 +58,8 @@ def main():
 			if (int(input_num) > 0) && (output_num > 0): #TODO check that this works
 				input_num = int(input_num)
 				output_num = int(output_num)
+				bits.append(input_num)
+				bits.append(output_num)
 				break
 		print("Inputs must be integers greater than 0")
 	# logic expressions
@@ -66,7 +75,9 @@ def main():
 		else:
 			print("File {} doesnt exist.".format(blif_file_path))
 			print("Please try again.")
-	# bitstream
+		#TODO append equations to bits
+		#bits.append('F'+str(equations))
+	# bitstream #TODO move this to front
 	while True:
 		print("Do you have a existing design in the form of a bitstream file?")
 		print("A: Yes  B: No")
