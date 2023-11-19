a2d829cdb926700198c739d0e7a6be1341b032d7
yt <o@dm.com>
2023-11-19 13:29:54 -0500

    changed ignores

--- test1.py
+++ test1.py
@@ -1,50 +1,10 @@
 from logic_synthesizer import *
 from minimize import *
-from FPGAstructure import *
-import networkx as nx
-from FPGAinitializer import get_equations
 
-#========tESTING eqns stuff
-#filename="test_eqns.blif"
-eqns=get_equations()
-print(eqns)
-#eqns=textToArray(eqns)
-#eqns=minimize_equations(eqns)
-eqns=toFactorer(eqns)
-print("Factorer")
-print(eqns)
-eqns=toSplitter(eqns)
-print("Splitter")
-print(eqns)
-
-#===============FPGA
-#FPGA1=FPGA()
-#FPGA1.set_LUTS(eqns)
-#print("LUTS")
-#print(FPGA1.get_nodes())
-#print("Nodes before J")
-#print(FPGA1.get_node_before('J'))
-
-#print("Inputs")
-#FPGA1.updateInputs()
-#print(FPGA1.get_nodes())
-#FPGA1.updateOutputs()
-
-#IN LIBRARY
-#print("Nodes")
-#print(FPGA1.get_nodes())
-#for node, data in FPGA1.nodes(data=True):
-#	print(f"Node {node}: {data}")
-#print(list(FPGA1.nodes()))
-
-#METHODS
-#print("LUTS")
-#print(FPGA1.get_LUTS())
-#print("inputs")
-#print(FPGA1.get_inputs())
-#print("outputs")
-#print(FPGA1.get_outputs())
-#FPGA1.show_FPGA()
+#========tESTING ARRAY
+filename="test_eqns.blif"
+names=textToArray(filename)
+equations=minimized_sop(names)
 
 #=========testing dont_care
 #letters=['*','1' ,'1']
