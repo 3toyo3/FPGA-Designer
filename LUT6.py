95ebfe3a05bed11ca6594c8b988bc5cb27c73ba9
kellthom <115281479+kellthom@users.noreply.github.com>
2023-11-20 20:14:47 -0500

    Add files via upload

--- LUT6.py
+++ LUT6.py
@@ -1,10 +1,11 @@
 class LUT6:
-    def __init__(self,name,inputs,output):
+    def __init__(self,name,inputs,output,ext):
 
         if len(inputs) <=6:
             self.name= name
             self.inputs=inputs
             self.output=output
+            self.external_output=ext
         else:
             print("Error: trying to assign more than 6 inputs to 6-input LUT")
 
